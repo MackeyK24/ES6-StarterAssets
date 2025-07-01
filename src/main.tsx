8561a36eacd3f2c1398f410f6c16a634855879b6
MackeyK24 <MackeyK24@gmail.com>
2025-06-30 22:26:52 -1000

    8.14.50 Update

--- src/main.tsx
+++ src/main.tsx
@@ -2,15 +2,16 @@ import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
 
-// ES6 Side Effects
-import "@babylonjs/core/Lights/Shadows/shadowGeneratorSceneComponent";
-import "@babylonjs/core/Debug/debugLayer.js"; // Augments the scene with the debug methods
-import "@babylonjs/inspector";
+// Default Side Effects
+import '@babylonjs/core/Lights/Shadows/shadowGeneratorSceneComponent';
+import '@babylonjs/core/Rendering/depthRendererSceneComponent';
+import '@babylonjs/loaders/glTF';
+//import '@babylonjs/inspector';
 
-// Starter Content
-import "@babylonjs-toolkit/dlc/DefaultCameraSystem";
-import "@babylonjs-toolkit/dlc/DebugInformation";
-import "@babylonjs-toolkit/dlc/MobileInputController";
+// Starter Content Side Effects
+import '@babylonjs-toolkit/dlc/DebugInformation';
+import '@babylonjs-toolkit/dlc/DefaultCameraSystem';
+import '@babylonjs-toolkit/dlc/MobileInputController';
 
 createRoot(document.getElementById('root')!).render(
     <App />
