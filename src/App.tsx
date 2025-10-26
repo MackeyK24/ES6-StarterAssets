9097312b5af7efbc5218c43456a1ee9ff2e86ca9
MackeyK24 <MackeyK24@gmail.com>
2025-10-25 21:21:04 -1000

    8.33.0 Template Updates

--- src/App.tsx
+++ src/App.tsx
@@ -1,42 +1,22 @@
 import { Scene } from "@babylonjs/core/scene";
 import { Vector3 } from "@babylonjs/core/Maths/math.vector";
 import { FreeCamera } from "@babylonjs/core/Cameras/freeCamera";
-import { HemisphericLight } from "@babylonjs/core/Lights/hemisphericLight";
 import { AssetsManager } from "@babylonjs/core/Misc/assetsManager";
 import { TransformNode } from "@babylonjs/core/Meshes/transformNode";
-import { HavokPlugin } from "@babylonjs/core/Physics/v2/Plugins/havokPlugin";
-import HavokPhysics from "@babylonjs/havok";
 import { SceneManager } from "@babylonjs-toolkit/next";
 import { ThirdPersonPlayerController } from "@babylonjs-toolkit/dlc/ThirdPersonPlayerController";
+import GameManager from "./Global";
 import Viewer from "./Viewer";
 import "./App.css";
 
 function App() {
   const onSceneReady = async (scene:Scene) => {
-    const defaultCamera = new FreeCamera("defaultCamera", new Vector3(0, 5, -10), scene);
-    defaultCamera.setTarget(Vector3.Zero());
-    scene.activeCamera = defaultCamera;
-
     ///////////////////////////////////////////////////////////////////////////////////////////////////////
     // STEP 1 - Initializes the runtime and global scene properties
     ///////////////////////////////////////////////////////////////////////////////////////////////////////
-    await SceneManager.InitializeRuntime(scene.getEngine(), { showDefaultLoadingScreen: true, hideLoadingUIWithEngine: false });
-    await import("@babylonjs-toolkit/dlc/DebugInformation");
-    await import("@babylonjs-toolkit/dlc/DefaultCameraSystem");
-    await import("@babylonjs-toolkit/dlc/MobileInputController");
-    if (import.meta.env.DEV) await import("@babylonjs/inspector");
-    
-    // @ts-ignore - This initializes fresh physics for this scene
-    globalThis.HK = await HavokPhysics();
-    globalThis.HKP = new HavokPlugin(false);
-    scene.enablePhysics(new Vector3(0,-9.81,0), globalThis.HKP);
-
-    // This cleans up globals when the scene is disposed
-    const cleanupGlobals = () => {
-        if (globalThis["HKP"]) delete globalThis["HKP"];
-        if (globalThis["HK"]) delete globalThis["HK"];
-    };
-    scene.onDisposeObservable.addOnce(cleanupGlobals);
+    const defaultCamera = new FreeCamera("defaultCamera", new Vector3(0, 5, -10), scene);
+    defaultCamera.setTarget(Vector3.Zero());
+    await GameManager.InitializeRuntime(scene, defaultCamera);
     
     ////////////////////////////////////////////////////////////////////////////////////////////////////////
     // STEP 2 - The loads the sample scene & player armature exported from the unity starter assets project
---
+++ src/Global.ts
@@ -0,0 +1,31 @@
+import { Scene } from "@babylonjs/core/scene";
+import { Vector3 } from "@babylonjs/core/Maths/math.vector";
+import { FreeCamera } from "@babylonjs/core/Cameras/freeCamera";
+import { HavokPlugin } from "@babylonjs/core/Physics/v2/Plugins/havokPlugin";
+import HavokPhysics from "@babylonjs/havok";
+import { SceneManager } from "@babylonjs-toolkit/next";
+
+class GameManager {
+    public static async InitializeRuntime(scene:Scene, defaultCamera: FreeCamera): Promise<void> {
+        await SceneManager.InitializeRuntime(scene.getEngine(), { showDefaultLoadingScreen: true, hideLoadingUIWithEngine: false });
+        await import("@babylonjs-toolkit/dlc/DebugInformation");
+        await import("@babylonjs-toolkit/dlc/DefaultCameraSystem");
+        await import("@babylonjs-toolkit/dlc/MobileInputController");
+        if (import.meta.env.DEV) await import("@babylonjs/inspector");
+        
+        // @ts-ignore - This initializes fresh physics for this scene
+        globalThis.HK = await HavokPhysics();
+        globalThis.HKP = new HavokPlugin(false);
+        scene.enablePhysics(new Vector3(0,-9.81,0), globalThis.HKP);
+
+        // This cleans up globals when the scene is disposed
+        const cleanupGlobals = () => {
+            if (globalThis["HKP"]) delete globalThis["HKP"];
+            if (globalThis["HK"]) delete globalThis["HK"];
+        };
+        scene.onDisposeObservable.addOnce(cleanupGlobals);
+
+    }
+}
+
+export default GameManager;
