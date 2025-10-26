2be261277db8ce2e143fc7909ea5482ed7ac8a93
MackeyK24 <MackeyK24@gmail.com>
2025-10-25 20:03:00 -1000

    8.33.0 Package Updates

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,78 @@
-import { Routes, Route } from "react-router-dom";
-import Viewer from "./Viewer";
-import DemoScene from "./Demo";
 import { Scene } from "@babylonjs/core/scene";
 import { Vector3 } from "@babylonjs/core/Maths/math.vector";
 import { FreeCamera } from "@babylonjs/core/Cameras/freeCamera";
 import { HemisphericLight } from "@babylonjs/core/Lights/hemisphericLight";
+import { AssetsManager } from "@babylonjs/core/Misc/assetsManager";
+import { TransformNode } from "@babylonjs/core/Meshes/transformNode";
+import { HavokPlugin } from "@babylonjs/core/Physics/v2/Plugins/havokPlugin";
+import HavokPhysics from "@babylonjs/havok";
+import { SceneManager } from "@babylonjs-toolkit/next";
+import { ThirdPersonPlayerController } from "@babylonjs-toolkit/dlc/ThirdPersonPlayerController";
+import Viewer from "./Viewer";
 import "./App.css";
 
 function App() {
   const onSceneReady = async (scene:Scene) => {
-    // This creates and positions a debug camera (non-mesh)
-    const camera = new FreeCamera("camera1", new Vector3(0, 5, -10), scene);
-    camera.setTarget(Vector3.Zero());
-    scene.activeCamera = camera;
+    const defaultCamera = new FreeCamera("defaultCamera", new Vector3(0, 5, -10), scene);
+    defaultCamera.setTarget(Vector3.Zero());
+    scene.activeCamera = defaultCamera;
 
-    // This creates ambient light, aiming 0,1,0 - to the sky (non-mesh)
-    const light = new HemisphericLight("light1", new Vector3(0, 1, 0), scene);
-    light.intensity = 0.7;
+    ///////////////////////////////////////////////////////////////////////////////////////////////////////
+    // STEP 1 - Initializes the runtime and global scene properties
+    ///////////////////////////////////////////////////////////////////////////////////////////////////////
+    await SceneManager.InitializeRuntime(scene.getEngine(), { showDefaultLoadingScreen: true, hideLoadingUIWithEngine: false });
+    await import("@babylonjs-toolkit/dlc/DebugInformation");
+    await import("@babylonjs-toolkit/dlc/DefaultCameraSystem");
+    await import("@babylonjs-toolkit/dlc/MobileInputController");
+    if (import.meta.env.DEV) await import("@babylonjs/inspector");
+    
+    // @ts-ignore - This initializes fresh physics for this scene
+    globalThis.HK = await HavokPhysics();
+    globalThis.HKP = new HavokPlugin(false);
+    scene.enablePhysics(new Vector3(0,-9.81,0), globalThis.HKP);
 
-    // This loads the demo starter assets scene
-    await DemoScene.Load(scene);
+    // This cleans up globals when the scene is disposed
+    const cleanupGlobals = () => {
+        if (globalThis["HKP"]) delete globalThis["HKP"];
+        if (globalThis["HK"]) delete globalThis["HK"];
+    };
+    scene.onDisposeObservable.addOnce(cleanupGlobals);
+    
+    ////////////////////////////////////////////////////////////////////////////////////////////////////////
+    // STEP 2 - The loads the sample scene & player armature exported from the unity starter assets project
+    // https://assetstore.unity.com/packages/essentials/starter-assets-character-controllers-urp-267961
+    ////////////////////////////////////////////////////////////////////////////////////////////////////////
+    const assetsManager = new AssetsManager(scene);
+    assetsManager.addMeshTask("samplescene", null, "/scenes/", "samplescene.gltf");
+    assetsManager.addMeshTask("playerarmature", null, "/scenes/", "playerarmature.gltf");
+    await SceneManager.LoadRuntimeAssets(assetsManager, ["samplescene.gltf", "playerarmature.gltf"], ()=> {
+
+        /////////////////////////////////////////////////////////////////////////////////////////////////////
+        // STEP 3 - Attach the player controller to the player armature
+        /////////////////////////////////////////////////////////////////////////////////////////////////////
+        try {
+            const player = scene.getNodeByName("PlayerArmature") as TransformNode;
+            if (player != null) {
+                const controller = new ThirdPersonPlayerController(player, scene, { arrowKeyRotation: true, smoothMotionSpeed:true, smoothChangeRate: 25.0 });
+                controller.enableInput = true;
+                controller.attachCamera = true;
+                controller.moveSpeed = 5.335;
+                controller.walkSpeed = 2.0;
+                controller.jumpSpeed = 12.0;
+            }
+        } catch (e) {
+            console.error("Failed to attach player controller", e);
+        } finally {
+            SceneManager.HideLoadingScreen(scene.getEngine());
+            SceneManager.FocusRenderCanvas(scene);
+        }
+    });
   };
 
   return (    
-    <Routes>
-      <Route path="/" element={
-        <div className="root">
-          <Viewer webgpu={true} antialias={true} adaptToDeviceRatio={true} onSceneReady={onSceneReady} className="canvas" />
-        </div>
-      } />
-    </Routes>
+    <div className="root">
+      <Viewer webgpu={true} antialias={true} adaptToDeviceRatio={true} onSceneReady={onSceneReady} className="canvas" />
+    </div>
   );
 }
 
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,12 @@
+import { MemoryRouter, Routes, Route } from "react-router-dom"
 import { createRoot } from "react-dom/client"
-import { BrowserRouter } from "react-router-dom"
-import App from "./App.tsx"
+import App from "./App"
 import "./index.css"
 
 createRoot(document.getElementById("root")!).render(
-    <BrowserRouter>
-        <App />
-    </BrowserRouter>
+  <MemoryRouter>
+    <Routes>
+      <Route path="/" element={<App />} />
+    </Routes>
+  </MemoryRouter>
 )
