dcd019b9138b0271488f24d03a6276d766c9c906
MackeyK24 <MackeyK24@gmail.com>
2025-11-10 08:25:07 -1000

    8.36.101 Starter Template Update

--- src/app.tsx
+++ src/app.tsx
@@ -2,12 +2,14 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import ProtectedRoute from "./router.tsx";
 import Babylon from "./pages/babylon.tsx";
 import Demo from "./pages/playground.tsx";
+import Home from "./home.tsx";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Demo />} />
+        <Route path="/" element={<Home />} />
+        <Route path="/demo" element={<Demo />} />
         <Route path="/babylon" element={<ProtectedRoute><Babylon allowQueryParams={true} /></ProtectedRoute>} />
       </Routes>
     </BrowserRouter>
