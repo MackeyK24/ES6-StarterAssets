b40b3237142bda4c4cd8f993be6b25eeabf9ce07
MackeyK24 <MackeyK24@gmail.com>
2025-11-10 14:25:29 -1000

    8.36.110 Babylon Routing Updates

--- vite.config.ts
+++ vite.config.ts
@@ -15,7 +15,7 @@ export default defineConfig(({ mode }) => ({
       output: {
         entryFileNames: "[name].js",
         assetFileNames: "[name].[ext]",
-        inlineDynamicImports: true,
+        inlineDynamicImports: false,
       }
     }
   },
